b38438abe9f11fce460c34a9cf22559f125f4984
Oscar Mulder <omulder@student.codam.nl>
2021-10-11 13:51:14 +0200

    remove forbidden functions

--- ex08/ft_rot_n.ml
+++ ex08/ft_rot_n.ml
@@ -6,35 +6,26 @@
 (*   By: omulder <omulder@student.codam.nl>           +#+                     *)
 (*                                                   +#+                      *)
 (*   Created: 2021/10/11 09:34:11 by omulder       #+#    #+#                 *)
-(*   Updated: 2021/10/11 12:02:12 by omulder       ########   odam.nl         *)
+(*   Updated: 2021/10/11 13:47:40 by omulder       ########   odam.nl         *)
 (*                                                                            *)
 (* ************************************************************************** *)
 
 let is_upper c =
-  Char.code c >= Char.code 'A' && Char.code c <= Char.code 'Z'
+  int_of_char c >= int_of_char 'A' && int_of_char c <= int_of_char 'Z'
   
 let is_lower c =
-  Char.code c >= Char.code 'a' && Char.code c <= Char.code 'z'
+  int_of_char c >= int_of_char 'a' && int_of_char c <= int_of_char 'z'
 
-let get_new_char c n =
-  let loweralpha = "abcdefghijklmnopqrstuvwxyz" in
-  let upperalpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" in
+let get_new_char n c =
   if is_upper c then
-    String.get upperalpha (((Char.code c) - (Char.code ('A')) + (n mod 26)) mod 26)
+    char_of_int (int_of_char 'A' + ((int_of_char c - int_of_char ('A') + (n mod 26)) mod 26))
   else if is_lower c then
-    String.get loweralpha (((Char.code c) - Char.code ('a') + (n mod 26)) mod 26)
+    char_of_int (int_of_char 'a' + ((int_of_char c - int_of_char ('a') + (n mod 26)) mod 26))
   else
     c
 
 let ft_rot_n n str =
-  let rec loop n str i =
-    let c = String.get str i in
-    if i == (String.length str) - 1 then
-      String.make 1 (get_new_char c n)
-    else
-      String.make 1 (get_new_char c n) ^ (loop n str (i + 1))
-  in
-  loop n str 0
+  String.map (get_new_char n) str
 
 let main () =
   print_string "ft_rot_n 1 \"abcdefghijklmnopqrstuvwxyz\": ";
